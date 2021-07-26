fbdfc80948f56bddbef6a046d44853cefba23a8b
Ch1ppyone <Ch1ppyone@mail.ru>
2021-07-27 01:30:34 +0300

    Обновлен функционал

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import argparse
 import os.path
+import json
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('filename', nargs='?', type=str, default="wine2.xlsx" )
+parser.add_argument('filename', nargs='?', type=str, default="wine.xlsx")
 args = parser.parse_args()
 
 env = Environment(
@@ -16,11 +18,18 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-excel_data_df = pd.read_excel(args.filename,  header=None, skiprows=1,  names=None,   na_values=[
+df = pd.read_excel(args.filename,  header=None, skiprows=1,  names=None,   na_values=[
                               'N/A', 'NA'], keep_default_na=False)
 
+
+grouped= defaultdict(list)
+
+for d in df.to_dict('records'):
+   grouped[d[0]].append(d)
+
+
 rendered_page = template.render(
-    data=excel_data_df
+    data=grouped
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
